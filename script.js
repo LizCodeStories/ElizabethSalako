ead4805a59f17f71908990f413e0a02cefdcd224
Elizabeth Salako <106440322+LizSalako@users.noreply.github.com>
2023-05-24 14:13:32 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -28,4 +28,42 @@ document.querySelectorAll(".nav-link").forEach(n => n.
       }
     });
 
-    
+    // Loading screen
+   // Wait for the page to finish loading
+window.addEventListener('load', function() {
+  // Set the delay time in milliseconds (8 seconds = 8000 milliseconds)
+  var delayTime = 200;
+
+  // Get a reference to the loading screen element
+  var loadingScreen = document.getElementById('loading-screen');
+
+  // Delay the removal of the loading screen
+  setTimeout(function() {
+    // Remove the loading screen by hiding or removing it from the DOM
+    loadingScreen.style.display = 'none';
+    // Alternatively, you can remove the loading screen element from the DOM completely:
+    // loadingScreen.parentNode.removeChild(loadingScreen);
+  }, delayTime);
+});
+
+
+
+//Navbar
+
+document.addEventListener('DOMContentLoaded', function() {
+  var textElement = document.querySelector('.opening');
+  var text = textElement.innerText;
+  textElement.innerText = '';
+
+  var i = 0;
+  var typingEffect = setInterval(function() {
+    if (i < text.length) {
+      textElement.innerText += text.charAt(i);
+      i++;
+    } else {
+      clearInterval(typingEffect);
+    }
+  }, 100);
+});
+
+
